2bf59eea5c01b55b8aee9ccaca49a942c2a399f7
Slava Ilnytskyy <slava.ilyntskyy@alumni.uleth.ca>
2019-03-27 15:49:23 -0600

    Fixed map_stats_bam.sh, now it calculates the mapping rate correctly

--- script/map_stats_bam.sh
+++ script/map_stats_bam.sh
@@ -8,7 +8,7 @@ do
     samplename=${filename%.bam}
     samtools flagstat $file > stat.tmp
     total=`awk 'NR==1 {print}' stat.tmp | awk '{print $1}'`
-    mapped=`awk 'NR==2 {print}' stat.tmp | awk '{print $1}'`
+    mapped=`awk 'NR==5 {print}' stat.tmp | awk '{print $1}'`
     percent_mapped=`bc <<< "scale=3; 100-($mapped/$total)*100"`
     echo -e "$samplename\t$total\t$mapped\t$percent_mapped"
     rm stat.tmp
